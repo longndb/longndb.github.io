8673d1c2cb897868d16b6f4e3c9c37fc80ca3350
longndb <longndb@vng.com.vn>
2020-07-22 17:35:31 +0700

    Removed all files that are in the .gitignore

--- vendor/bundle/ruby/2.6.0/gems/jekyll-highlight-1.0.6/lib/jekyll-highlight.rb
+++
@@ -1,8 +0,0 @@
-# rubocop:disable Style/FileName
-require "jekyll-highlight/version"
-require "jekyll-highlight/highlight_tag"
-
-module Jekyll
-  module Highlight
-  end
-end
--- vendor/bundle/ruby/2.6.0/gems/jekyll-highlight-1.0.6/lib/jekyll-highlight/version.rb
+++
@@ -1,5 +0,0 @@
-module Jekyll
-  module Highlight
-    VERSION = "1.0.6".freeze
-  end
-end
